8caaf4965160ead67e20071b9635a65ad1597f8b
黃子軒 <s1071938@gm.pu.edu.tw>
2021-05-27 20:38:45 +0800

    water

---
+++ Web410719388/Controllers/WaterController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410719388.Controllers
+{
+    public class WaterController : Controller
+    {
+        // GET: Water
+        public ActionResult Index(string name, float? weight)
+        {
+            var result = "";
+
+            if (weight.HasValue)
+            {
+                result = (weight * 30) + "cc";
+            }
+
+            ViewBag.name = name;
+            ViewBag.result = result;
+
+            return View();
+        }
+    }
+}
