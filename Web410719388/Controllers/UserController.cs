e493e72d722b7c247bfe5f928ed62ab706de9676
黃子軒 <s1071938@gm.pu.edu.tw>
2021-06-03 20:55:52 +0800

    add model

--- Web410719388/Controllers/UserController.cs
+++ Web410719388/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web410719388.ViewModels;
 
 namespace Web410719388.Controllers
 {
@@ -12,32 +13,32 @@ namespace Web410719388.Controllers
         // GET: User
         public ActionResult SignUp()
         {
-            return View();
+            return View(new SignUpData());
         }
 
         [HttpPost]
-        public ActionResult SignUp(string name, string account, string password)
+        public ActionResult SignUp(SignUpData data)
         {
-            if (name.IsNullOrWhiteSpace())
+            if (data.Name.IsNullOrWhiteSpace())
             {
                 ViewBag.NameMessage = "請輸入姓名";
             }
-            if (account.IsNullOrWhiteSpace())
+            if (data.Account.IsNullOrWhiteSpace())
             {
                 ViewBag.AccountMessage = "請輸入帳號";
             }
-            if (password.IsNullOrWhiteSpace())
+            if (data.Password.IsNullOrWhiteSpace())
             {
                 ViewBag.PasswordMessage = "請輸入密碼";
             }
 
-            if (!name.IsNullOrWhiteSpace() &&
-               !account.IsNullOrWhiteSpace() &&
-               !password.IsNullOrWhiteSpace()) {
+            if (!data.Name.IsNullOrWhiteSpace() &&
+               !data.Account.IsNullOrWhiteSpace() &&
+               !data.Password.IsNullOrWhiteSpace()) {
                 ViewBag.Message = "註冊成功";
             }
 
-            return View();
+            return View(data);
         }
     }
 }
